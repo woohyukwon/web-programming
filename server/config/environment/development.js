cc0aa1e908d74d6d11250f96f003ae94e5fb645c
Liangyin Yu <liayu@cs.du.edu>
2020-03-06 04:27:20 +0000

    Completed lab 16

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -6,9 +6,8 @@ module.exports = {
     // MongoDB connection options
     mongo: {
         useMongoClient: true,
-      uri: process.env.MONGODB_URI || 'mongodb://web2-mongodb/web2-liayu-
-      git add .
-        git commit -m "Completed lab 16"
-      git push -u origin auth    // Seed database on startup
+        uri: process.env.MONGODB_URI || 'mongodb://web2-mongodb/web2-liayu-auth'
+    },
+    // Seed database on startup
     seedDB: true,
 };
